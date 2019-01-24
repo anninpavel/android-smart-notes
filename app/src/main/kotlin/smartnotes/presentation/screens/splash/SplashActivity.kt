00ac544df1cde6fe90d8945d4323f2f9701fbdb9
Pavel Annin <anninpavel2014@gmail.com>
2019-01-25 00:32:36 +0300

    added splash screen; changed app icons; added app theme

---
+++ app/src/main/kotlin/smartnotes/presentation/screens/splash/SplashActivity.kt
@@ -0,0 +1,10 @@
+package smartnotes.presentation.screens.splash
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Экран загрузки приложения.
+ *
+ * @author Pavel Annin (https://github.com/anninpavel).
+ */
+class SplashActivity : AppCompatActivity()
